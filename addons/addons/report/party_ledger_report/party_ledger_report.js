d8da014da80a939f5638334312189acc91d079a3
bobzz-zone <bobzz.zone@gmail.com>
2016-05-30 20:02:57 +0700

    test

--- addons/addons/report/party_ledger_report/party_ledger_report.js
+++ addons/addons/report/party_ledger_report/party_ledger_report.js
@@ -3,6 +3,30 @@
 
 frappe.query_reports["Party Ledger Report"] = {
 	"filters": [
-
+		{
+			"fieldname":"from",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"reqd":1
+		},
+		{
+			"fieldname":"to",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": get_today(),
+			"reqd":1
+		},
+		{
+			"fieldname":"name",
+			"label": __("Name"),
+			"fieldtype": "Data"
+		},
+		{
+			"fieldname":"type",
+			"label": __("Type"),
+			"fieldtype": "Select",
+			"options":{"All","Customer","Supplier"}
+		}
 	]
 }
--- addons/addons/report/party_ledger_report/party_ledger_report.py
+++ addons/addons/report/party_ledger_report/party_ledger_report.py
@@ -5,5 +5,15 @@ from __future__ import unicode_literals
 import frappe
 
 def execute(filters=None):
-	columns, data = [], []
+	columns, data = ["Posting Date:Date:200","Name:Data:200","Total:Currency:200","Summary:Text:400","Type:Data:200","Voucher No:Data:200"], []
+	#data = frappe.db.sql("""select gl.posting_date,ifnull(dn.customer_name,pr.supplier_name),sum(gl.debit),  from `tabGL Entry` gl 
+	#	left join `tabDelivery Note` dn on gl.voucher_type="Delivery Note" and gl.voucher_no =dn.name and (dn.customer like "%{0}%" or dn.customer_name like "%{0}%")
+	#	left join `tabPurchase Receipt` pr on gl.voucher_type="Purchase Receipt" and gl.voucher_no =pr.name  and (pr.supplier like "%{0}%" or pr.supplier_name like "%{0}%") 
+	#	where gl.voucher_type IN ("Purchase Receipt","Delivery Note") and gl.debit>0 group by gl.voucher_no
+	#	""".format(filters.get("name")))
+	if filters.get("type")=="All" :
+		dn=frappe.db.sql("""select dn.posting_date,dn.supplier_name,dn.base_grand_total,concat(group_concat(concat(di.item_code,"->",di.item_name,"=",di.qty)),dn.awb_no) ,"Delivery Note",dn.name
+			from `tabDelivery Note` dn left join `tabDelivery Note Item` di on di.parent=dn.name 
+			where dn.posting_date between "{0}" and "{1}" and (dn.customer like "%{2}%" or dn.customer_name like "%{2}%") group by dn.name
+			""")
 	return columns, data
