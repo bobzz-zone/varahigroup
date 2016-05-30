87446cd2af7b07ca08451600576df94c458cc3f2
bobzz-zone <bobzz.zone@gmail.com>
2016-05-31 01:01:41 +0700

    menus

--- addons/config/addons.py
+++ addons/config/addons.py
@@ -2,7 +2,28 @@ from __future__ import unicode_literals
 from frappe import _
 
 def get_data():
-	return [
+	return [{
+			"label": _("Billing"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Sales Invoice",
+					"description": _("Bills raised to Customers.")
+				},
+				{
+					"type": "doctype",
+					"name": "Purchase Invoice",
+					"description": _("Bills raised by Suppliers.")
+				},
+				{
+					"type": "doctype",
+					"name": "Payment Request",
+					"description": _("Payment Request")
+				},
+				
+			]
+
+		},
 		{
 			"label": _("Report"),
 			"icon": "icon-list",
@@ -12,6 +33,17 @@ def get_data():
 					"name": "Party Ledger Report",
 					"is_query_report": True,
 					"doctype":"Account"
+				},{
+					"type": "report",
+					"name": "Accounts Receivable",
+					"doctype": "Sales Invoice",
+					"is_query_report": True
+				},
+				{
+					"type": "report",
+					"name": "Accounts Payable",
+					"doctype": "Purchase Invoice",
+					"is_query_report": True
 				},
 			]
 		}]
