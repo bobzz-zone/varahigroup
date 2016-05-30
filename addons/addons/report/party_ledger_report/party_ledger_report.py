022b9dcb756b248c08a449029242700dad82e84a
bobzz-zone <bobzz.zone@gmail.com>
2016-05-31 00:01:05 +0700

    tes

--- addons/addons/report/party_ledger_report/party_ledger_report.py
+++ addons/addons/report/party_ledger_report/party_ledger_report.py
@@ -6,8 +6,8 @@ import frappe
 
 def execute(filters=None):
 	columns, data = ["Posting Date:Date:200","Name:Data:200","Total:Currency:200","Summary:Text:400","Type:Data:200","Voucher No:Data:200"], []
-	data=frappe.db.sql("""select dn.posting_date,dn.customer_name,dn.base_grand_total,concat(group_concat(concat(di.item_code,"->",di.item_name,"=",di.qty)),dn.awb_no) ,"Delivery Note",dn.name 
+	data=frappe.db.sql("""select dn.posting_date,dn.customer_name,dn.base_grand_total,group_concat(di.item_code) ,group_concat(di.qty),dn.name 
 			from `tabDelivery Note` dn left join `tabDelivery Note Item` di on di.parent=dn.name 
-			 group by dn.name 
+			where dn.posting_date between "{0}" and "{1}" and (dn.customer like "%{2}%" or dn.customer_name like "%{2}%") group by dn.name 
 		""".format(filters.get("from"),filters.get("to"),filters.get("name")),as_list=1)
 	return columns, data
