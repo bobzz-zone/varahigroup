f7691e24996d75f2cf2223b3550bb4a5ee18e058
bobzz-zone <bobzz.zone@gmail.com>
2016-06-01 19:03:40 +0700

    code test

--- addons/addons/report/account_receivable_detail/account_receivable_detail.js
+++ addons/addons/report/account_receivable_detail/account_receivable_detail.js
@@ -3,6 +3,54 @@
 
 frappe.query_reports["Account Receivable Detail"] = {
 	"filters": [
-
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"reqd": 1
+		},
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"reqd": 1,
+			"width": "60px"
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1,
+			"width": "60px"
+		},
+		
+		{
+			"fieldtype": "Break",
+		},
+		{
+			"fieldname":"party_type",
+			"label": __("Party Type"),
+			"fieldtype": "Select",
+			"options": ["Customer", "Supplier"],
+			"default": "Customer"
+		},
+		{
+			"fieldname":"party",
+			"label": __("Party"),
+			"fieldtype": "Dynamic Link",
+			"reqd":1,
+			"get_options": function() {
+				var party_type = frappe.query_report.filters_by_name.party_type.get_value();
+				var party = frappe.query_report.filters_by_name.party.get_value();
+				if(party && !party_type) {
+					frappe.throw(__("Please select Party Type first"));
+				}
+				return party_type;
+			}
+		}
 	]
 }
--- addons/addons/report/party_ledger_report/party_ledger_report.js
+++ addons/addons/report/party_ledger_report/party_ledger_report.js
@@ -4,30 +4,53 @@
 frappe.query_reports["Party Ledger Report"] = {
 	"filters": [
 		{
-			"fieldname":"from",
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"reqd": 1
+		},
+		{
+			"fieldname":"from_date",
 			"label": __("From Date"),
 			"fieldtype": "Date",
-			"width": "80",
-			"reqd":1
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"reqd": 1,
+			"width": "60px"
 		},
 		{
-			"fieldname":"to",
+			"fieldname":"to_date",
 			"label": __("To Date"),
 			"fieldtype": "Date",
-			"default": get_today(),
-			"reqd":1
+			"default": frappe.datetime.get_today(),
+			"reqd": 1,
+			"width": "60px"
+		},
+		
+		{
+			"fieldtype": "Break",
 		},
 		{
-			"fieldname":"name",
-			"label": __("Name"),
-			"fieldtype": "Data"
-		},
-		{
-			"fieldname":"type",
-			"label": __("Type"),
+			"fieldname":"party_type",
+			"label": __("Party Type"),
 			"fieldtype": "Select",
-			"options":["All","Customer","Supplier"],
-			"default":"All"
+			"options": ["Customer", "Supplier"],
+			"default": "Customer"
+		},
+		{
+			"fieldname":"party",
+			"label": __("Party"),
+			"fieldtype": "Dynamic Link",
+			"reqd":1,
+			"get_options": function() {
+				var party_type = frappe.query_report.filters_by_name.party_type.get_value();
+				var party = frappe.query_report.filters_by_name.party.get_value();
+				if(party && !party_type) {
+					frappe.throw(__("Please select Party Type first"));
+				}
+				return party_type;
+			}
 		}
 	]
 }
